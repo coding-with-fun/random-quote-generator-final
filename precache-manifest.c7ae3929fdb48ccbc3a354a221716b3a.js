a4c57c9d60863393719e35f56dfc2abb26382ca1
coding-with-fun <harsh2124p@gmail.com>
2020-10-29 21:35:16 +0530

    Updates

---
+++ precache-manifest.c7ae3929fdb48ccbc3a354a221716b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e2ca4d76a8458a71d42e97ead7170e",
+    "url": "/mern-authenticator-final/index.html"
+  },
+  {
+    "revision": "629ae47e9a9d6fdfde4d",
+    "url": "/mern-authenticator-final/static/css/main.d66a4799.chunk.css"
+  },
+  {
+    "revision": "518705b9712cd1478ca6",
+    "url": "/mern-authenticator-final/static/js/2.b5b67733.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/mern-authenticator-final/static/js/2.b5b67733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629ae47e9a9d6fdfde4d",
+    "url": "/mern-authenticator-final/static/js/main.e216af38.chunk.js"
+  },
+  {
+    "revision": "41df703a3ac8ce45b526",
+    "url": "/mern-authenticator-final/static/js/runtime-main.ccbd0a1f.js"
+  }
+]);

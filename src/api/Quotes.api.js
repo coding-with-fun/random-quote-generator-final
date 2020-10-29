4ab6aef7df16670d176f7eec518b901d1fd6b4a8
coding-with-fun <harsh2124p@gmail.com>
2020-10-29 21:26:18 +0530

    Updates

--- src/api/Quotes.api.js
+++
@@ -1,7 +0,0 @@
-import Axios from "axios";
-
-const BASE_URL = "https://api.quotable.io";
-
-export const fetchRandomQuote = (limit, skip) => {
-    return Axios.get(BASE_URL + "/quotes?limit=" + limit + "&skip=" + skip);
-};
